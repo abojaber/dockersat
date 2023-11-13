667572e552443d55c6aa534407c11324d7b9386a
Fahad Al-Hantushi <abojaber@programmer.net>
2023-11-13 11:55:32 +0000

    use migrate file to execute file in /mssql/scripts folder which mounted to container

--- mssql/builder/migrate.sh
+++ mssql/builder/migrate.sh
@@ -1,9 +1,8 @@
 #/bin/bash
 
-echo "Run /data/scripts.sql file only when build the image..."
+echo "Import file $1 file "
 
 
 #/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -q "CREATE DB myDB"
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -q "CREATE DB myDB"
-
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i "/scripts/$1.sql"
